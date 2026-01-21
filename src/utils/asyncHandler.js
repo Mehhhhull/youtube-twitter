3f013fe4f34d80ff97154702996eceb0bb64c79c
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-01-21 21:05:41 +0530

    Bugs Fixed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // Using Promices
 
 const asyncHandler= (requestHandler)=>{
-  (req,res,next)=>{
+  return (req,res,next)=>{
     Promise.resolve(requestHandler(req,res,next)).catch(next).catch((error)=>next(err))
   }
 }
