3c50f848a0136592f449a940844c867bfc0b4007
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-01-29 23:23:26 +0530

    Get current user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -260,4 +260,12 @@ const changeCurrentPassword=asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+const getCurrentUser=asyncHandler(async(req,res)=>{
+  return res
+  .status(200)
+  .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
+})
+
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser}
