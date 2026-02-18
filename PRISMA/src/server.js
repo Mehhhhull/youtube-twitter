e6505175894135d1f919fbd55b846a0316b0554a
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-02-18 15:02:27 +0530

    ..

--- PRISMA/src/server.js
+++ PRISMA/src/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express=require('express');
+const authorRoutes=require('./routes/authorRoutes');
+
+
 
 const app=express();
 app.use(express.json());
+
+app.use('/api/author',authorRoutes);
 
 const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>console.log(`Server is now running at port ${PORT}`))
