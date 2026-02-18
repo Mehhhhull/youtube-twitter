ea2c53c1e6165e6d43c20bf037053c57d0f3c6fd
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-02-18 15:28:17 +0530

    ..

--- PRISMA/src/controllers/bookController.js
+++ PRISMA/src/controllers/bookController.js
@@ -1,1 +1,41 @@
-const book
+const bookService=require('../services/bookService');
+
+exports.addBook=async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    res.status(400).json({error:error.message})
+  }
+}
+
+exports.getAllBooks=async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    res.status(400).json({error:error.message})
+  }
+}
+
+exports.getBookById=async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    res.status(400).json({error:error.message})
+  }
+}
+
+exports.updateBook=async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    res.status(400).json({error:error.message})
+  }
+}
+
+exports.deleteBook=async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    res.status(400).json({error:error.message})
+  }
+}
--- PRISMA/src/services/bookService.js
+++ PRISMA/src/services/bookService.js
@@ -20,4 +20,86 @@ async function addBook(title,authorId,publishedData){try {
   throw error;
 }}
 
-module.exports={addBook};
+async function getAllBooks(){
+  try {
+    const books=await prisma.book.findMany({
+      include:{author:true}
+    })
+    return books;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getBookById(id) {
+  try {
+    const book=await prisma.book.findUnique({
+      where :{id},
+      include:{author:true}
+    })
+    if(!book){
+      throw new Error('Book not found');
+    }
+    return book;
+  } catch (error) {
+    throw error;
+  }
+  
+}
+
+async function updateBook(id,newTitle){
+  try {
+    // const book=await prisma.book.findUnique({
+    //   where :{id},
+    //   include:{author:true}
+    // })
+    // if(!book){
+    //   throw new Error('Book not found');
+    // }
+
+    // const updatedBook=await prisma.book.update({
+    //   where:{id},
+    //   data:{title:newTitle},
+    //   include:{author:true}
+    // })
+    // return updatedBook;
+    
+    //using transactions
+    const updatedBook=await prisma.$transaction(async(prisma)=>{
+      const book=await prisma.book.findUnique({where:{id}})
+      if(!book){
+         throw new Error('Book not found');
+      }
+
+      return prisma.book.update({
+        where:{id},
+        data:{
+          title:newTitle,
+        },
+        include:{
+          author:true
+        }
+      })
+    })
+
+    return updatedBook;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteBook(id){
+  try {
+    const deletedBook=await prisma.book.delete({
+      where:{id},
+      include:{author:true}
+    });
+
+    return deleteBook
+  } catch (error) {
+    throw error
+  }
+}
+
+
+module.exports={addBook,getAllBooks,getBookById,updateBook,deleteBook};
